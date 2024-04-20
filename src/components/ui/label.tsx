9c55a25f23124bb656582dbdc74a868263fa1d2a
Nathan Rothenberger <nroth10@gmail.com>
2024-04-20 15:46:11 -0500

    wip label refactor

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,26 +1,51 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-)
+/**
+ * Label
+ * ---
+ * A row that holds a checkbox or radio, and a label
+ */
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+const _typography = "font-sans text-md font-normal leading-none text-form";
+const _disabled = "peer-disabled:cursor-not-allowed peer-disabled:opacity-70";
 
-export { Label }
+const labelVariants = cva([_typography, _disabled]);
+
+type LabelRef = React.ElementRef<typeof LabelPrimitive.Root>;
+
+type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+  VariantProps<typeof labelVariants>;
+
+const Label = React.forwardRef<LabelRef, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(labelVariants(), className)}
+      {...props}
+    />
+  )
+);
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label };
+
+/**
+ * Label Wrapper
+ * ---
+ * A row that holds a checkbox or radio, and a label
+ */
+const styles = "flex items-center space-x-3";
+
+interface LabelWrapProps extends React.ComponentProps<"div"> {
+  children: React.ReactNode;
+}
+
+export const LabelWrap = ({ children }: LabelWrapProps) => (
+  <div className={styles}>{children}</div>
+);
