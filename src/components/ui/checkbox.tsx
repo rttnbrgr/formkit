df65269e6dd227a7c173e8944182c6b9c0167106
Nathan Rothenberger <nroth10@gmail.com>
2024-04-20 15:45:48 -0500

    wip - checkbox refactor

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,30 +1,53 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { Check } from "lucide-react"
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import { Check } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
-      className
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
+/**
+ * TODO:
+ * -----
+ * Error styles
+ *
+ */
+
+/**
+ * Checkbox Container
+ */
+const _box = "group peer shrink-0 h-7 w-7 p-0.5";
+const _border = "border-2 border-dashed border-form";
+const _focus =
+  "ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2";
+const _checked = "";
+// const _checked = "data-[state=checked]:bg-interactive data-[state=checked]:text-interactive-foreground data-[state=checked]:border-none";
+const _disabled = "disabled:cursor-not-allowed disabled:opacity-50";
+
+/**
+ * Checkbox Indicator
+ */
+const _innerBox = "flex items-center justify-center w-full h-full";
+const _innerChecked = "text-current bg-interactive text-interactive-foreground";
+
+type CheckboxRef = React.ElementRef<typeof CheckboxPrimitive.Root>;
+type CheckboxProps = React.ComponentPropsWithoutRef<
+  typeof CheckboxPrimitive.Root
+>;
+
+const Checkbox = React.forwardRef<CheckboxRef, CheckboxProps>(
+  ({ className, ...props }, ref) => (
+    <CheckboxPrimitive.Root
+      ref={ref}
+      className={cn(_box, _border, _focus, _checked, _disabled, className)}
+      {...props}
     >
-      <Check className="h-4 w-4" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-))
-Checkbox.displayName = CheckboxPrimitive.Root.displayName
+      <CheckboxPrimitive.Indicator className={cn([_innerBox, _innerChecked])}>
+        <Check className="h-3.5 w-3.5" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+);
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
-export { Checkbox }
+export { Checkbox };
