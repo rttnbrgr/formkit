a3884a78f76f2558378b651c6cca6363d71e4bd5
Nathan Rothenberger <nroth10@gmail.com>
2024-04-28 15:10:19 -0500

    wip: cleanup marketing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,75 +5,37 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 import { ProfileForm } from "@/components/form-demo";
 import { ContactForm } from "@/components/contact-form";
+import { Button } from "@/components/ui/button";
 
+// add semantic bg colors
+// Do you need non-semantic tailwind help?
+// md:bg-blue-500 lg:bg-green-500 xl:bg-purple-500
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-center p-24 bg-grey1">
-      <div className="flex flex-col gap-10 ">
-        {/* Checkbox */}
-        <div>hi</div>
-        <div className="flex flex-col gap-4 bg-indigo-500">
-          <div className="text-blackk font-sans text-lg font-normal leading-snug">
-            By filling out this form, you agree that you have read the Config
-            FAQ.
+    <main className="flex min-h-screen flex-col justify-start bg-grey1 px-8 py-12 sm:bg-red-500 sm:justify-center ">
+      <div className="flex flex-col items-center justify-center gap-12 bg-black/40">
+        <div className="flex flex-col items-center justify-center gap-6 max-w-[800px]">
+          {/* Logo */}
+          <div className="w-[228px] h-'[68px] flex gap-4">
+            <div className="h-5 rounded bg-black w-full" />
+            <div className="w-5 h-5 rounded bg-pink-500" />
           </div>
-          <LabelWrap>
-            <Checkbox id="terms" />
-            <Label htmlFor="terms">Yes</Label>
-          </LabelWrap>
-        </div>
-
-        {/* Input */}
-        <div>before</div>
-        {/* <div className="flex flex-col gap-4 ">
-          <WrappedInput />
-        </div>
-        <div className="flex flex-col gap-4 ">
-          <WrappedInput placeholder="Placeholder" />
-        </div> */}
-        <div className="flex flex-col gap-4 ">
-          <input />
-        </div>
-        <div className="flex flex-col gap-4 ">
-          <Label size="small">Label</Label>
-          <Label size="default">Label</Label>
-        </div>
-
-        {/* RadioGroup */}
-        <div className="flex flex-col gap-4 ">
-          <div className="text-blackk font-sans text-lg font-normal leading-snug">
-            I consent to Figma sharing my name and company name with Figma’s
-            Config sponsors for marketing purposes. Note: You may also choose to
-            share your contact information with specific sponsors at Config by
-            letting them scan your attendee badge.
+          {/* Copy */}
+          <div className="typography-2">
+            Baby vaporware brunch lomo bodega boys distillery kogi, air plant
+            cupping subway tile fingerstache tumeric cardigan tacos +1.
+            Farm-to-table bespoke freegan meggings sus lomo four loko. Taxidermy
+            polaroid mlkshk salvia ethical. Squid intelligentsia listicle,
+            humblebrag fixie drinking vinegar jawn subway tile venmo. Art party
+            forage ethical fanny pack grailed beard cardigan wayfarers paleo
+            mixtape. aajdkfja;skfjasdk;
           </div>
-          <RadioGroup defaultValue="comfortable">
-            <LabelWrap>
-              <RadioGroupItem value="default" id="r1" />
-              <Label htmlFor="r1">Yes</Label>
-            </LabelWrap>
-            <LabelWrap>
-              <RadioGroupItem value="comfortable" id="r2" />
-              <Label htmlFor="r2">No</Label>
-            </LabelWrap>
-          </RadioGroup>
+        </div>
+        <div className="flex flex-col gap-4 bg-green-300 w-full max-w-[390px] sm:flex-row sm:w-auto sm:max-w-fit">
+          <Button className="w-full">Tell Me More</Button>
+          <Button>Get Figma File</Button>
         </div>
       </div>
-      {/* foo */}
-      <div className="border border-white w-full rounded-lg py-8 px-4 my-4">
-        <div className="flex flex-col gap-4">
-          <div className="flex flex-row gap-4 ">
-            {/* <WrappedInput className="grow" />
-            <WrappedInput className="grow" placeholder="Placeholder" /> */}
-          </div>
-          {/* <WrappedInput /> */}
-        </div>
-      </div>
-      <ProfileForm />
-      <div className="mt-8"></div>
-      <hr />
-      <div className="mt-8"></div>
-      <ContactForm />
     </main>
   );
 }
