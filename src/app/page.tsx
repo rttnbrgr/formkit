191f69c8df6e5d62f0861f67780ca7b0116ed69c
Nathan Rothenberger <nroth10@gmail.com>
2024-05-13 19:49:36 -0500

    include env vars for links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,15 @@ const debugResponsive =
 // write copy
 // remove old pages
 export default function Home() {
+  const figmaLink = process.env.NEXT_PUBLIC_LINK_FIGMA;
+  const mailLink = process.env.NEXT_PUBLIC_LINK_MAIL;
+
+  if (!mailLink || !figmaLink) {
+    return;
+  }
+
+  figmaLink;
+
   return (
     <main className="flex min-h-screen flex-col justify-start px-8 py-12 sm:justify-center">
       <div className="flex flex-col items-center justify-center gap-12 ">
@@ -31,10 +40,15 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 w-full max-w-[390px] sm:flex-row sm:w-auto sm:max-w-fit">
           <Button className="w-full" variant="outline" asChild>
-            <a href="mailto:foo@gmail.com">Tell Me More</a>
+            <Link
+              target="_blank"
+              href={`${mailLink}?subject=Tell me more about Formkit! 🚀`}
+            >
+              Tell Me More
+            </Link>
           </Button>
           <Button asChild>
-            <Link href="/demo">Get Figma File</Link>
+            <Link href={figmaLink}>Get Figma File</Link>
           </Button>
         </div>
       </div>
